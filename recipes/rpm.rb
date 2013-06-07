b4cc5adf09fcfd680a35b0f472128c5e9493d580
Ben Plunkert <ben.plunkert@blackboard.com>
2013-06-07 15:29:56 -0700

    fixing up openfire::rpm

--- recipes/rpm.rb
+++ recipes/rpm.rb
@@ -1,7 +1,19 @@
 local_rpm_path = "#{Chef::Config[:file_cache_path]}/#{node[:openfire][:rpm_file]}"
 
-rpm_package local_rpm_path do
+remote_file local_rpm_path do
   source "http://www.igniterealtime.org/downloadServlet?filename=openfire/#{node[:openfire][:rpm_file]}"
-  action :install
+  not_if "rpm -q openfire"
+  notifies :install, "rpm_package[openfire]", :immediately
+end
+
+rpm_package "openfire" do
+  source local_rpm_path
+  only_if {::File.exists?(local_rpm_path)}
+  action :nothing
+end
+
+file "openfire-rpm-cleanup" do
+  path local_rpm_path
+  action :delete
 end
 
