2f48fee989678dabecd1ae450d96f9e97d224bc3
Ben Plunkert <ben.plunkert@blackboard.com>
2013-08-15 11:26:19 -0700

    another change to openfire:ssl

--- recipes/ssl.rb
+++ recipes/ssl.rb
@@ -3,7 +3,15 @@ cookbook_file "#{node[:openfire][:home_dir]}/KeyStoreImport.java" do
   source 'KeyStoreImport.java'
 end
 
-exec "javac KeyStoreImport.java" do 
+execute "javac KeyStoreImport.java" do 
   creates "#{node[:openfire][:home_dir]}/KeyStoreImport.class"
   cwd "#{node[:openfire][:home_dir]}"
+  action :run
 end
+
+execute "import_keys" do 
+  not_if "#{node[:java][:java_home]}/bin/keytool -list -keystore #{node[:openfire][:home_dir]}/resources/security/keystore -storepass \"#{node[:openfire][:ssl][:keystore_password]}\" | grep -q \"Your keystore contains [0-9] entries\""
+  command "java KeyStoreImport keystore #{node[:openfire][:ssl][:der_cert_chain]} #{node[:openfire][:ssl][:der_key]} \"#{node[:openfire][:ssl][:key_name]}\""
+  cwd "#{node[:openfire][:home_dir]}"
+end
+
